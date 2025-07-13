03cee2056e1bc70b11c25bdf0c511310def45562
Eduardo Mendoza Campos <emendoza9507@gmail.com>
2025-07-12 21:08:26 -0400

    Admin dashboard

--- app/Http/Controllers/DefaultController.php
+++ app/Http/Controllers/DefaultController.php
@@ -2,12 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Audition;
 use Illuminate\Contracts\View\View;
 
 class DefaultController extends Controller
 {
     public function privacy(): View
     {
-        return view('privacy');
+        $audition = Audition::query()->where('active', true)->orderBy('created_at', 'desc')->first();
+        return view('privacy', [
+            'audition' => $audition
+        ]);
     }
 }
